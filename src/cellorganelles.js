7140fadb593895033cdd63f8e8c1f3836f5980b7
sCherry1101 <cherry.1101.8113@gmail.com>
2026-05-09 15:03:02 +0530

    build attempt: using span for the 3d

--- src/cellorganelles.js
+++ src/cellorganelles.js
@@ -12,3 +12,7 @@ container.appendChild(renderer.domElement);
 const light = new THREE.AmbientLight(0xffffff, 1);
 scene.add(light);
 
+const geometry = new THREE.ShepreGeometry(1, 32,32);
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff00, wireframe: true });
+const cell = new THREE.Mesh(geometry, material);
+scene.add(cell);
